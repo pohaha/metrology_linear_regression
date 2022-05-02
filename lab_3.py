0b746fad14a43832486d1c5b0d0cff6779cf2d0f
moskalev-i <moskalev-i@protei.ru>
2022-05-02 21:27:26 +0300

    all files

---
+++ lab_3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# read and print the first few rows of the data
+data = pd.read_csv('src/epi_r.csv')
+print(data.head())
+
